be6d7ce80c0bd9bcedaac965762618bc4a9dc31f
mayo <mygreymatter@gmail.com>
2015-10-31 17:04:13 +0530

    initial commit

---
+++ app/src/main/java/com/mayo/openhouse/ActMain.java
@@ -0,0 +1,13 @@
+package com.mayo.openhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }
+}
---
+++ app/src/main/java/com/mayo/openhouse/OpenHouse.java
@@ -0,0 +1,7 @@
+package com.mayo.openhouse;
+
+/**
+ * Created by mayo on 31/10/15.
+ */
+public class OpenHouse {
+}
